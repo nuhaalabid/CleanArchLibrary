9d56380197462a7e0340f2311d5682077604a1a6
nuha <noha.q.hasan@gmail.com>
2024-12-10 14:01:30 +0100

    Änddringar i Database och andra klasser

--- Infrastructure/Database/RealDatabase.cs
+++ Infrastructure/Database/RealDatabase.cs
@@ -10,13 +10,12 @@ namespace Infrastructure.Database
 {
     public class Realdatabase : DbContext
     {
+        public Realdatabase() { }
         public Realdatabase(DbContextOptions<Realdatabase> options) : base(options) { }
 
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<User> Users { get; set; }
 
-    }  
-
-
+    }
 }
